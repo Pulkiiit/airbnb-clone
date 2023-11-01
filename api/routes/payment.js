6ecf647aa5e58dbc3ee0d2c410a295ce395886d3
Pulkiiit <kpulkit30@gmail.com>
2023-11-01 16:12:06 +0530

    payment rmoved

--- api/routes/payment.js
+++ api/routes/payment.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const razor = require("razorpay");
+const Razorpay = require("razorpay");
 const router = express.Router();
 const crypto = require("crypto");
 const Booking = require("../models/Booking");
 const Place = require("../models/Place");
+const { builtinModules } = require("module");
 
 let transactionCounter = 0;
+const secret_key = process.env.SECRET_KEY;
 
 function generateUniqueReceiptNumber() {
   const now = new Date();
@@ -17,50 +19,9 @@ function generateUniqueReceiptNumber() {
 
 const receiptNumber = generateUniqueReceiptNumber();
 
-router.post("/payment", async (req, res) => {
-  try {
-    const instance = new razor({
-      key_id: process.env.RAZORPAY_KEY_ID,
-      key_secret: process.env.RAZORPAY_SECRET,
-    });
-    const options = {
-      amount: req.body.price * req.body.days * 100,
-      currency: "INR",
-      receipt: receiptNumber,
-    };
-    const order = await instance.orders.create(options);
-    if (!order) return res.status(500).send("Some error occured");
-    res.json(order);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-router.post("/success", async (req, res) => {
-  try {
-    const {
-      orderCreationId,
-      razorpayPaymentId,
-      razorpayOrderId,
-      razorpaySignature,
-    } = req.body;
-
-    const shasum = crypto.createHmac("sha256", "w2lBtgmeuDUfnJVp43UpcaiT");
-
-    shasum.update(`${orderCreationId}|${razorpayPaymentId}`);
-
-    const digest = shasum.digest("hex");
-    if (digest !== razorpaySignature)
-      return res.status(400).json({ msg: "Transaction not legit!" });
-
-    res.json({
-      msg: "success",
-      orderId: razorpayOrderId,
-      paymentId: razorpayPaymentId,
-    });
-  } catch (e) {
-    res.status(500).send(e);
-  }
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_SECRET,
 });
 
 module.exports = router;
--- client/src/components/BookingBox.jsx
+++ client/src/components/BookingBox.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import axios from "axios";
-import logo from "../assets/logo.png";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 const BookingBox = ({ place, id, maxGuests }) => {
@@ -47,123 +46,49 @@ const BookingBox = ({ place, id, maxGuests }) => {
       setdisable(false);
     }
   };
-  function loadScript(src) {
-    return new Promise(resolve => {
-      const script = document.createElement("script");
-      script.src = src;
-      script.onload = () => {
-        resolve(true);
-      };
-      script.onerror = () => {
-        resolve(false);
-      };
-      document.body.appendChild(script);
-    });
-  }
-  async function displayRazorpay() {
-    const res = await loadScript(
-      "https://checkout.razorpay.com/v1/checkout.js"
-    );
-
-    if (!res) {
-      alert("Razorpay SDK failed to load. Are you online?");
-      return;
-    }
-
-    // creating a new order
-    const result = await axios.post("http://localhost:4000/book/payment", {
-      price: place.price,
-      days: days,
-    });
-
-    if (!result) {
-      alert("Server error. Are you online?");
-      return;
-    }
-
-    const { amount, id: order_id, currency } = result.data;
-
-    const options = {
-      key: import.meta.env.VITE_RAZORPAY_KEY,
-      amount: amount.toString(),
-      currency: currency,
-      name: "Airbnb",
-      description: "Test Transaction",
-      image: { logo },
-      order_id: order_id,
-      handler: async function (response) {
-        const data = {
-          orderCreationId: order_id,
-          razorpayPaymentId: response.razorpay_payment_id,
-          razorpayOrderId: response.razorpay_order_id,
-          razorpaySignature: response.razorpay_signature,
-        };
-
-        const result = await axios.post(
-          "http://localhost:4000/book/success",
-          data
-        );
-
-        alert(result.data.msg);
-      },
-      prefill: {
-        name: "Soumya Dey",
-        email: "SoumyaDey@example.com",
-        contact: "9999999999",
-      },
-      notes: {
-        address: "Soumya Dey Corporate Office",
-      },
-      theme: {
-        color: "#61dafb",
-      },
-    };
-
-    const paymentObject = new window.Razorpay(options);
-    paymentObject.open();
-  }
   return (
-    <div className='bg-white rounded-2xl p-4 shadow-md'>
-      <div className='text-xl text-center'>
-        Price: &#8377; {place.price} / per night
-      </div>
-      <div className='border rounded-2xl mt-4'>
-        <div className='flex'>
-          <div className='px-3 py-4 '>
-            <label>Check in</label>
-            <input
-              type='date'
-              value={from}
-              onChange={e => setFrom(e.target.value)}
-            />
+    <>
+      <div className='bg-white rounded-2xl p-4 shadow-md'>
+        <div className='text-xl text-center'>
+          Price: &#8377; {place.price} / per night
+        </div>
+        <div className='border rounded-2xl mt-4'>
+          <div className='flex'>
+            <div className='px-3 py-4 '>
+              <label>Check in</label>
+              <input
+                type='date'
+                value={from}
+                onChange={e => setFrom(e.target.value)}
+              />
+            </div>
+            <div className='px-3 py-4 border-l'>
+              <label>Check out</label>
+              <input
+                type='date'
+                value={to}
+                onChange={e => setTo(e.target.value)}
+              />
+            </div>
           </div>
-          <div className='px-3 py-4 border-l'>
-            <label>Check out</label>
-            <input
-              type='date'
-              value={to}
-              onChange={e => setTo(e.target.value)}
-            />
+          <div>
+            <div className='px-3 py-4'>
+              <label>No of guests</label>
+              <input type='number' ref={guests} onChange={guestHandler} />
+            </div>
           </div>
         </div>
-        <div>
-          <div className='px-3 py-4'>
-            <label>No of guests</label>
-            <input type='number' ref={guests} onChange={guestHandler} />
-          </div>
-        </div>
+        <button
+          disabled={disable}
+          onClick={() => {
+            bookingHandler();
+          }}
+          className='bg-primary w-full rounded-full px-2 py-2 text-white mt-4 disabled:bg-gray-500'
+        >
+          Book this place
+        </button>
       </div>
-      <button
-        disabled={disable}
-        onClick={() => {
-          displayRazorpay();
-          bookingHandler();
-        }}
-        className='bg-primary w-full rounded-full px-2 py-2 text-white mt-4 disabled:bg-gray-500'
-      >
-        Book this place
-      </button>
-    </div>
+    </>
   );
 };
 
